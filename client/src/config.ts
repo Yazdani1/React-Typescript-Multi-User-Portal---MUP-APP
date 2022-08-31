5d757216ec0ff8908e65ecf1c8caf6518f9170cf
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-08-31 20:22:21 +0300

    Deploy

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,6 @@
-export const API_URL ="http://localhost:8080/api"
+// export const API_URL ="http://localhost:8080/api"
 
+export const API_URL ="https://node-js-multi-user-portal-mup-aws-s3-aws-ses.vercel.app/api";
 
 export const headerConfig = () => {
   return {
